3a1106a9075dbcdf94ec9281bc7ecbfd680ac61b
rwbu69 <lovelyrwbu69@gmail.com>
2026-06-19 04:01:45 +0700

    style: customize default laravel email template to match cosurent branding

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block; font-size: 24px; font-weight: 900; letter-spacing: 4px; color: #18181b; text-decoration: none; text-transform: uppercase;">
+COSURENT
+</a>
+</td>
+</tr>
